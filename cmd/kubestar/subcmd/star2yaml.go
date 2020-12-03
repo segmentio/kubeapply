0d1b8bc354d21ff47eef3dce10108cf2fba4588a
Benjamin Yolken <benjamin.yolken@segment.com>
2020-12-02 21:39:01 -0800

    Keep iterating on kubestar conversion functions

--- cmd/kubestar/subcmd/star2yaml.go
+++ cmd/kubestar/subcmd/star2yaml.go
@@ -1,12 +1,57 @@
 package subcmd
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/segmentio/kubeapply/pkg/star/expand"
 	"github.com/spf13/cobra"
 )
 
-var applyCmd = &cobra.Command{
-	Use:   "star2yaml [cluster configs]",
-	Short: "apply runs kubectl apply over the resources associated with a cluster config",
-	Args:  cobra.MinimumNArgs(1),
-	RunE:  applyRun,
+var star2yamlCmd = &cobra.Command{
+	Use:   "star2yaml [star path]",
+	Short: "star2yaml expands a kube starlark file to YAML",
+	Args:  cobra.ExactArgs(1),
+	RunE:  star2yamlRun,
+}
+
+type star2yamlFlags struct {
+	valuesStr string
+}
+
+var star2yamlFlagValues star2yamlFlags
+
+func init() {
+	star2yamlCmd.Flags().StringVar(
+		&star2yamlFlagValues.valuesStr,
+		"values",
+		"",
+		"JSON values to use for starlark evaluation",
+	)
+
+	RootCmd.AddCommand(star2yamlCmd)
+}
+
+func star2yamlRun(cmd *cobra.Command, args []string) error {
+	values := map[string]interface{}{}
+
+	if star2yamlFlagValues.valuesStr != "" {
+		if err := json.Unmarshal([]byte(star2yamlFlagValues.valuesStr), &values); err != nil {
+			return err
+		}
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	result, err := expand.StarToYaml(args[0], cwd, values)
+	if err != nil {
+		return err
+	}
+	fmt.Println(result)
+
+	return nil
 }
--- cmd/kubestar/subcmd/yaml2star.go
+++ cmd/kubestar/subcmd/yaml2star.go
@@ -1,1 +1,58 @@
 package subcmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/segmentio/kubeapply/pkg/star/convert"
+	"github.com/spf13/cobra"
+)
+
+var yaml2starCmd = &cobra.Command{
+	Use:   "yaml2star [YAML configs]",
+	Short: "yaml2star converts one or more kube YAML manifests to starlark",
+	Args:  cobra.MinimumNArgs(1),
+	RunE:  yaml2starRun,
+}
+
+type yaml2StarFlags struct {
+}
+
+var yaml2StarFlagValues yaml2StarFlags
+
+func init() {
+	RootCmd.AddCommand(yaml2starCmd)
+}
+
+func yaml2starRun(cmd *cobra.Command, args []string) error {
+	filePaths := []string{}
+
+	for _, arg := range args {
+		paths, err := filepath.Glob(arg)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(paths)
+
+		for _, path := range paths {
+			info, err := os.Stat(path)
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				continue
+			}
+
+			filePaths = append(filePaths, path)
+		}
+	}
+
+	result, err := convert.YamlToStar(filePaths)
+	if err != nil {
+		return err
+	}
+	fmt.Println(result)
+	return nil
+}
